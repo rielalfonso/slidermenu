46bc754134019caf82b099e1671f81ee89639ada
Renmark Ayson <61658617+rielalfonso@users.noreply.github.com>
2020-03-03 01:37:38 +0800

    Add files via upload

---
+++ slideshow/java.js
@@ -0,0 +1,49 @@
+var slideIndex = 0;
+
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+   
+    slideIndex = 1
+  }    
+  for (i = 0; i < dots.length; i++) {
+   
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); 
+}
+
+showSlides();
+
+var slideIndex = 1;
+onclick(slideIndex);
+
+function plusSlides(n) {
+  onclick(slideIndex += n);
+}
+
+
+function onclick(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
